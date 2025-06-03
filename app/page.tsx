c4f62ba8ad20e7e2a8a10ef1d7a2de0c438fa721
andrey <andrejjchabanjuk@rambler.ru>
2025-06-03 17:30:42 +0300

    last changes 03 06 25

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { Categories, Container } from '@/components/shared'
+import { Container, Filters} from '@/components/shared'
 import { Title } from '@/components/shared/title'
-import { Button } from '@/components/ui/button'
+import { TopBar } from '@/components/shared/top-bar'
+
 
 
 export default function Home() {
@@ -8,7 +9,19 @@ export default function Home() {
     <>
       <Container className='mt-10'>
         <Title text="Все пиццы" size="lg" className="font-extrabold"/>
-        <Categories />
+        <TopBar />
+        <Container className="pb-14 mt-10">
+          <div className="flex gap-[60px]">
+            <div className="w-[250px]">
+              <Filters />
+            </div>
+            <div className="flex-1">
+              <div className="flex flex-col gap-16">
+                Список товаров
+              </div>
+            </div>
+          </div>
+        </Container>
       </Container>
     </>
   )
