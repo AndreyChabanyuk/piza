cbd4ed4b52e4d08b8e985cea1af505f4e027abd0
andrey <andrejjchabanjuk@rambler.ru>
2025-06-05 17:28:08 +0300

    filters-checkbox show all

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -23,7 +23,7 @@ export const Categories: React.FC<Props> = ({ className }) => {
             {cats.map((cat, index) => ( 
                 <a className={cn("flex items-center font-bold h-11 rounded-2xl px-5",
                 activeIndex === index && 'bg-white shadow-md shadow-gray-200 text-primary'
-                )} key="index">
+                )} key={index}>
                     <button>{cat}</button>
                 </a>
             ))
---
+++ components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,62 @@
+'use client'
+import React from 'react'
+import { FilterCheckbox, FilterCheckboxProps } from './filter-checkbox'
+import { Input } from '../ui/input'
+
+
+type Item = FilterCheckboxProps
+
+interface Props {
+    title: string,
+    items: Item[],
+    defaultItems?: Item[],
+    limit?: number,
+    searchInputPlaceholder?: string,
+    onChange?: (values: string[]) => void,
+    defaultValue?: string[],
+    className?: string
+}
+
+
+
+export const CheckboxFiltersGroup: React.FC<Props> = (
+{ 
+   title,
+   items,
+   defaultItems,
+   limit = 5,
+   searchInputPlaceholder = 'Поиск...',
+   className,
+   onChange,
+   defaultValue,
+}
+) => {
+    
+    const [showAll, setShowAll] = React.useState(false)
+
+    return (
+        <div className={className}>
+
+            <p className="font-bold mb-3">
+                {title}
+            </p>
+
+            <div className="mb-5">
+                <Input placeholder={searchInputPlaceholder} className="bg-gray-50 border-none"/>
+            </div>
+
+            <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+                {items.map((item,index) => (
+                    <FilterCheckbox 
+                        onCheckedChange={(ids) => console.log(ids)}
+                        checked={false}
+                        key={index}
+                        text={item.text}
+                        value={item.value}
+                        endAdornment={item.endAdornment}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
